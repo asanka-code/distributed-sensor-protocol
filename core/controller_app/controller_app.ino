92c92c26d0c2caa7e7c651c2ec5688460f391371
asanka-code <asanka.code@gmail.com>
2015-06-17 15:07:03 +0530

    Controller app is changed to monitor CD (carrier detect) register without any transmission.

--- core/controller_app/controller_app.ino
+++ core/controller_app/controller_app.ino
@@ -40,89 +40,24 @@ void setup () {
 
 void loop () {
         
-        //-----------------------------------------------------------------------------------------------------------------
-        //#Asanka: experimenting with the received signal strength
-        byte carrier_detect_reg_value = (byte) 0;
-        byte dummy_byte_data = B10101010;
-        //-----------------------------------------------------------------------------------------------------------------
+        byte carrier_detect_reg_value = (byte) 0;        
+        static packet_struct packet;                                   
         
-        static packet_struct packet;
-        //int num_sensors = 2;
-        int num_sensors = 1;
-
-        for (int sensor_number = 0; sensor_number<num_sensors; sensor_number++) {
-                                               
-                // generating request packet
-                packet.system_id = 0;
-                packet.component_id = sensor_number;
-                packet.data = 0;
+         //RX mode
+         Mirf.ceHi();
+         Mirf.readRegister(CD, &carrier_detect_reg_value, sizeof(carrier_detect_reg_value));
+         Serial.print("carrier_detect_reg_value = ");
+         Serial.println(carrier_detect_reg_value, BIN);
                 
-                // sending request packet
-                Mirf.setTADDR((byte *)"disp"); 
-                Mirf.send((byte *) &packet);
-                // take note of the time we send this packet
-                unsigned long time = millis();
+          // process the received sensor data
+          Mirf.getData((byte *) &packet);
+          Serial.print("component ID = ");
+          Serial.println(packet.component_id);
+          Serial.print("data = ");
+          Serial.println(packet.data);
                 
-                // wait till the sending is done  
-                while (Mirf.isSending()) { }
-                Serial.print("Controller: Sent a request packet to the sensor:");
-                Serial.println(sensor_number);
-                            
-                //-----------------------------------------------------------------------------------------------------------------
-               //#Asanka: experimenting with the received signal strength
-               Mirf.readRegister(CD, &carrier_detect_reg_value, sizeof(carrier_detect_reg_value));
-               Serial.print("carrier_detect_reg_value = ");
-               Serial.println(carrier_detect_reg_value, BIN);
-               Serial.println(dummy_byte_data, BIN);
-               //-----------------------------------------------------------------------------------------------------------------             
-                            
-                // wait for the response data packet
-                while (!Mirf.dataReady()) {
-                        
-                        //-----------------------------------------------------------------------------------------------------------------
-                       //#Asanka: experimenting with the received signal strength
-                       //RX mode
-                       Mirf.ceHi();
-                       Mirf.readRegister(CD, &carrier_detect_reg_value, sizeof(carrier_detect_reg_value));
-                       Serial.print("carrier_detect_reg_value = ");
-                       Serial.println(carrier_detect_reg_value, BIN);
-                       Serial.println(carrier_detect_reg_value, HEX);
-                       Serial.println(dummy_byte_data, BIN);
-                       //-----------------------------------------------------------------------------------------------------------------  
-                        
-                        // it no response for a 1 second, we start sensing cycle again.
-                        // ToDo: actually what should happpen here is moving to the next iteration of the for loop. But putting 'continue' will continue the above while
-                        // loop instead of continuing outer for loop. So I put 'return' to goto the next iteration of the loop() cycle for the moment. Do something about it later!!
-                        //if ((millis() - time) > 1000) {
-                        if ((millis() - time) > 500) {
-                                return;
-                        }
-                }
-               Serial.print("Controller: Received a data packet from the sensor:");
-               Serial.println(sensor_number);
-  
-               //-----------------------------------------------------------------------------------------------------------------
-               //#Asanka: experimenting with the received signal strength
-               //RX mode
-               Mirf.ceHi();
-               Mirf.readRegister(CD, &carrier_detect_reg_value, sizeof(carrier_detect_reg_value));
-               Serial.print("carrier_detect_reg_value = ");
-               Serial.println(carrier_detect_reg_value, BIN);
-               Serial.println(dummy_byte_data, BIN);
-               //-----------------------------------------------------------------------------------------------------------------  
-               
-                
-                // process the received sensor data
-                Mirf.getData((byte *) &packet);
-                Serial.print("component ID = ");
-                Serial.println(packet.component_id);
-                Serial.print("data = ");
-                Serial.println(packet.data);
-                
-                // wait for a while before sampling the sensors in the next iteration
-                //delay(1000);
-                delay(500);
-        }
+           // wait for a while before sampling the sensors in the next iteration                
+           delay(500);
 } 
   
   
